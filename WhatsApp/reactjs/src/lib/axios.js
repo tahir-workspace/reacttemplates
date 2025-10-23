4e720ede68d46ba38ab56c0195c9abc2cef32924
tahir-krank <ti@krank.com>
2025-10-23 11:49:51 +0500

    added jwt in header

--- WhatsApp/reactjs/src/lib/axios.js
+++ WhatsApp/reactjs/src/lib/axios.js
@@ -1,7 +1,19 @@
 import axios from "axios";
 import config from "../config";
+import Cookies from "js-cookie";
 
 export const axiosInstance = axios.create({
   baseURL: config.serverBaseUrl + "/api",
   withCredentials: true,
 });
+
+axiosInstance.interceptors.request.use(
+  (req) => {
+    const token = Cookies.get("jwt");
+    if (token) {
+      req.headers.Authorization = `Bearer ${token}`;
+    }
+    return req;
+  },
+  (err) => Promise.reject(err)
+);
